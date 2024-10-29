6036c6aa88ba6a549da3bfc5b9cf10af7e0fb4a1
kevingil <kevin_gil@live.com>
2024-10-29 16:56:36 -0700

    remove features that don't need redux

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,17 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '@/features/counter/counterSlice';
 import authReducer from '@/features/auth/authSlice';
-import blogReducer from '@/features/blog/blogSlice';
-import profileReducer from '@/features/profile/profileSlice';
-import storageReducer from '@/features/storage/storageSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
-    blog: blogReducer,
-    profile: profileReducer,
-    storage: storageReducer,
   }
 });
 
