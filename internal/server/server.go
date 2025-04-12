c4b5d6e126f32183c9b0a8945568f08ddd485a17
Kevin Gil <kevin@sellscale.com>
2025-04-12 01:58:05 -0700

    auth service

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,23 +4,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 
 	"blog-agent/internal/database"
+	user "blog-agent/internal/services/user/services"
 )
 
 type FiberServer struct {
 	*fiber.App
 
-	db database.Service
+	db          database.Service
+	authService *user.AuthService
 }
 
-func New() *FiberServer {
-	server := &FiberServer{
+func New(secretKey string) *FiberServer {
+	db := database.New()
+
+	return &FiberServer{
 		App: fiber.New(fiber.Config{
 			ServerHeader: "blog-agent",
 			AppName:      "blog-agent",
 		}),
-
-		db: database.New(),
+		db:          db,
+		authService: user.NewAuthService(db, secretKey),
 	}
-
-	return server
 }
