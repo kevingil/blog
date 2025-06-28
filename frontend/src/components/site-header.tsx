7ee81df4baa2548f148a8e92b08eb477e295a281
Kevin Gil <kevin@sellscale.com>
2025-06-28 01:56:10 -0700

    update dashboard

--- frontend/src/components/site-header.tsx
+++ frontend/src/components/site-header.tsx
@@ -1,8 +1,27 @@
-import { Button } from "@/components/ui/button"
+
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 
+import { isAuthenticatedAtom, useAuth } from '@/services/auth/auth';
+import { Link } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+
+import { useAtomValue } from "jotai";
+import { getAboutPage } from "@/services/user";
+import { Button } from "./ui/button";
+
+
 export function SiteHeader() {
+  const { token } = useAuth();
+  const isAuthenticated = useAtomValue(isAuthenticatedAtom);
+
+  const { data: pageData, isLoading: aboutPageLoading } = useQuery({
+    queryKey: ['aboutPage'],
+    queryFn: getAboutPage,
+    staleTime: 5000,
+    enabled: !!isAuthenticated && !!token,
+  });
+  console.log("navbar pageData", pageData, aboutPageLoading);
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -13,16 +32,11 @@ export function SiteHeader() {
         />
         <h1 className="text-base font-medium">Documents</h1>
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-            <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="dark:text-foreground"
-            >
-              GitHub
-            </a>
+        <Link to="/">
+          <Button variant="outline" size="sm">
+            Home
           </Button>
+        </Link>
         </div>
       </div>
     </header>
