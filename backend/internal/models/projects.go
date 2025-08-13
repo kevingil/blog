144a6775786d90e228d7d5bbec9f3dfac3d578e0
Kevin Gil <kevin@sellscale.com>
2025-08-12 23:31:15 -0700

    add project content and tag ids

--- backend/internal/models/projects.go
+++ backend/internal/models/projects.go
@@ -4,16 +4,19 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 type Project struct {
-	ID          uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	Title       string    `json:"title" gorm:"not null"`
-	Description string    `json:"description" gorm:"type:text;not null"`
-	ImageURL    string    `json:"image_url"`
-	URL         string    `json:"url"`
-	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ID          uuid.UUID     `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	Title       string        `json:"title" gorm:"not null"`
+	Description string        `json:"description" gorm:"type:text;not null"`
+	Content     string        `json:"content" gorm:"type:text"`
+	TagIDs      pq.Int64Array `json:"tag_ids" gorm:"type:integer[]"`
+	ImageURL    string        `json:"image_url"`
+	URL         string        `json:"url"`
+	CreatedAt   time.Time     `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time     `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 func (Project) TableName() string {
