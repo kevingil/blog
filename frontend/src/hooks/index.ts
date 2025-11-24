9f53d795521745e93a94d4fe9ef6a531bb157f4e
Kevin Gil <kevin@sellscale.com>
2025-11-23 22:54:24 -0800

    use session hook

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,11 @@
+// Central export for all custom hooks
+export { useSession } from './use-session';
+export type { 
+  UseSessionOptions, 
+  UseSessionReturn, 
+  SessionState,
+  ChatMessage,
+  SearchResult,
+  SourceInfo
+} from './use-session';
+
